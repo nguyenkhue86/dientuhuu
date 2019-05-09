5ea7a45e485f903e8330c1512a2727f14ddb604c
Nguyen Tran <ttrongnguyen@tma.com.vn>
2019-05-09 10:21:48 +0700

    clean code

--- src/main/java/tma/com/vn/dts/reponsitory/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package tma.com.vn.dts.reponsitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tma.com.vn.dts.model.ImageEntity;
-
-@Repository
-public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
-}
